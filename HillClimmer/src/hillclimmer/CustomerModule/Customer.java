7adf009b5635b9baccbd52f731ff3375f5755077
Lye Wei Lun <74344714+Dellrall@users.noreply.github.com>
2025-08-31 17:19:50 +0800

    Cleaned up some junk file within the project

--- HillClimmer/src/hillclimmer/CustomerModule/Customer.java
+++ HillClimmer/src/hillclimmer/CustomerModule/Customer.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package hillclimmer.DurationModule;
+package hillclimmer.CustomerModule;
 
 /**
  *
  * @author las
  */
-public class Reminder {
+public class Customer {
     
 }
